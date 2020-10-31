abff29e8cf6a698d95705320ec2ddaae467b07fe
manticorevault <artur_seilah@hotmail.com>
2020-10-31 14:52:08 +0000

    Fixing some requirements

--- src/App.js
+++ src/App.js
@@ -30,6 +30,21 @@ function App() {
       <div>
         <button className="button-reset" onClick={() => setCount(0)}> Resetar </button>
       </div>
+
+
+      <div className="Contagem">
+        <h1>
+          Contagem
+      </h1>
+      </div>
+      <div className="number">{count}</div>
+
+      <button className="button-success" onClick={() => setCount(counter => counter + 1)}> <FiChevronsUp /> </button>
+      <button className="button-failure" onClick={() => setCount(counter => counter - 1)}> <FiChevronsDown /> </button>
+
+      <div>
+        <button className="button-reset" onClick={() => setCount(0)}> Resetar </button>
+      </div>
     </div>
   )
 
